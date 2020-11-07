05927e08d99cac95ad899de00859f96b3ea6523b
Daniel Soto <dasg.1993@gmail.comm>
2020-11-07 17:37:11 -0500

    [feature] - add user action & reducer in to the store & create user List component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import CakeContainer from './components/CakeContainer'
-import HookCakeContainer from './components/HookCakeContainer'
-import IceCreamContainer from './components/IceCreamContainer'
-import NewCakeContainer from './components/NewCakeContainer'
-import ItemContainer from './components/ItemContainer'
+// import CakeContainer from './components/CakeContainer'
+// import HookCakeContainer from './components/HookCakeContainer'
+// import IceCreamContainer from './components/IceCreamContainer'
+// import NewCakeContainer from './components/NewCakeContainer'
+// import ItemContainer from './components/ItemContainer'
+import UsersContainer from './components/UsersContainer'
 import store from './redux/store'
 import './App.css';
 
@@ -12,12 +13,13 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <ItemContainer cake />
+        {/* <ItemContainer cake />
         <ItemContainer />
         <CakeContainer />
         <HookCakeContainer />
         <IceCreamContainer />
-        <NewCakeContainer />
+        <NewCakeContainer /> */}
+        <UsersContainer />
       </div>
     </Provider>
   );
