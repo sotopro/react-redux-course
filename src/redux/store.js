c6f65e2f798da8431d1335beb0f5a9144f9e093e
Daniel Soto <dasg.1993@gmail.comm>
2020-11-07 16:07:20 -0500

    [feature] - setup composedevtools & applymiddleware logger & thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import cakeReducer from './cake/cakeReducer'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
-const store = createStore(cakeReducer)
+const store = createStore(
+    cakeReducer,
+    composeWithDevTools(applyMiddleware(logger, thunk))
+    )
 
 export default store;
